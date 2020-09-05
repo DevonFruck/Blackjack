e9a69546adae5b518f13f4542a4066e3ba081597
Devon Fruck <59775369+DevonFruck@users.noreply.github.com>
2020-09-05 13:54:52 -0400

    Add files via upload

--- Deck.cpp
+++ Deck.cpp
@@ -1,11 +1,59 @@
 #include "Deck.hpp"
+#include <vector>
+#include <string>
 
 Deck::Deck() {
-  int count=0;
 
-  for (int i=0; i<4; i++) {
-    deck[count] = new Card(s);
+  int count=0;  //Keep track of number of cards put in
+  int value = 2;
+
+  // Putting number value cards in the deck
+  for (int i=0; i<9; i++) {
+    for (int j=0; j<4; j++) {
+      deck[count] = new Card(suits[j], faces[i], value);
+      count++;
+    }
+    value ++;
+  }
+  // Putting face cards in the deck
+  for (int l=9; l<=11; l++) {
+    for (int m=0; m<4; m++) {
+      deck[count] = new Card(suits[m], faces[l], 10);
+      count ++;
+    }
+  }
+  // Putting Aces in the deck
+  for (int k=0; k<4; k++) {
+    deck[count] = new Card(suits[k], "Ace", 11);
+    count ++;
   }
 
+  shuffle();
+}
 
+
+void Deck::shuffle() {
+
+  srand((unsigned int)time(NULL));
+  int maxIter = 1000 + rand()%500;  //random number of shuffles
+
+  for (int iter=0; iter < maxIter; iter++) {
+    // rand1 and rand2 are positions in the deck that will
+    // swap places to simulate shuffling of the deck
+    int rand1 = rand() % 52;
+    int rand2 = rand() % 52;
+
+    Card* ptr = deck[rand1];
+    deck[rand1] = deck[rand2];
+    deck[rand2] = ptr;
+
+    top = &deck[0];
+  }
+}
+
+
+Card* Deck::topCard() {
+  Card* ptr = *top;
+  top += 1;
+  return ptr;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
 #include "Hand.hpp"
 #include "Card.hpp"
+#include "Deck.hpp"
+#include <vector>
 
 int main() {
-  Hand* dealer = new Hand();
+  //Hand* dealer = new Hand();
+  //std::cout << dealer->getCard1()->getFace() << "\n" << dealer->getCard2()->getFace() << "\n";
 
-  std::cout << dealer->getCard1()->getFace() << "\n" << dealer->getCard2()->getFace() << "\n";
+  std::cout << "Welcome to the BlackJack table. \n";
+
+  int wager;
+  std::cout << "Enter how much you would like to wager: ";
+  std::cin >> wager;
+
+  Deck* deck = new Deck();
+
+  Hand* dealer = new Hand(0, deck->topCard(), deck->topCard());
+  Hand* player = new Hand(wager, deck->topCard(), deck->topCard());
+
+  int hit_or_stand;
+  while (player->isPlaying() == 1 && player->getTotalValue() <= 21) {
+    std::cout << "1 for hit, 2 for stand: ";
+    std::cin >> hit_or_stand;
+
+    std::cout << player->getTotalValue() << "\n";
+
+    if (hit_or_stand == 1) {
+      std::cout<<"\nYou've chosen to hit.\n";
+      player->hit(deck->topCard());
+    }
+    else if (hit_or_stand == 2) {
+      std::cout<<"\nYou've chosen to stand.\n";
+      player->stand();
+      break;
+    }
+    else {
+      std::cout << "Invalid choice. Try again\n";
+    }
+  }
+  std::cout << player->getTotalValue() << "\n";
+
+std::cout << "\n\nNow it's the dealer's turn.\n\n";
+  // Dealer's turn
+
+  while (dealer->isPlaying() && dealer->getTotalValue() <= 21) {
+    if (dealer->getTotalValue() < 17) {
+      dealer->hit(deck->topCard());
+    }
+    else {
+      dealer->stand();
+    }
+  }
+
 }
